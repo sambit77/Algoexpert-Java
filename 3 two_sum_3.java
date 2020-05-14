cd46107c65cfdf667708dc12a2aca22d3fe527e6
Sambit Pradhan <46903174+sambit77@users.noreply.github.com>
2020-05-14 20:48:44 +0530

    using hashmap

--- 3 two_sum_3.java
+++ 3 two_sum_3.java
@@ -10,6 +10,9 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [2, 71].*/
+//using hashmap
+//Time Complexity O(n)
+//Space Complsexity O(n)
 import java.util.*;
 class A
 {
