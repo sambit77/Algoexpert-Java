a4189917629e39b7ab1b38b9cbcb0bd25aeb5a46
Sambit Pradhan <46903174+sambit77@users.noreply.github.com>
2020-05-14 20:45:35 +0530

    bruteforce way

--- 1 two_sum.java
+++ 1 two_sum.java
@@ -10,7 +10,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [2, 7].*/
-
+//BRUTEFORCE METHOD
 // Time Complexity O(n*n)
 //Space Complexity O(1)
 import java.util.*;
