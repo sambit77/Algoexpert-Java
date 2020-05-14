aff93b6a3d0f1444558f5a36da7f254bb4e19f66
Sambit Pradhan <46903174+sambit77@users.noreply.github.com>
2020-05-14 20:47:15 +0530

    using left,right pointers

--- 2 two_sum_2.java
+++ 2 two_sum_2.java
@@ -10,6 +10,10 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [2, 7].*/
+
+//USING LEFT RIGHT POINTERS
+//Time Compexity O(n log n)
+//Space complexity O(1)
 import java.util.*;
 class A
 {
