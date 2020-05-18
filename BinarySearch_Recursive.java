650a49412f7d5724f72e82d4548fc8b3cfe587c2
Sambit Pradhan <46903174+sambit77@users.noreply.github.com>
2020-05-19 03:10:21 +0530

    Not Space Optimized

---
+++ BinarySearch_Recursive.java
@@ -0,0 +1,40 @@
+//Time Complexity O(log n) | Space Complexity O(log n)
+import java.util.*;
+class A
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = new int[]{12,30,45,67,89,90,435,23}; //given array should be in sorted order
+		int target = 435; //element to find
+
+		int index = binarySearch(arr,target);
+		System.out.println("Given Arrray"+Arrays.toString(arr)); //prints the input array
+		System.out.println(target+" is found at "+index);
+	}
+	public static int binarySearch(int[] arr, int target)
+	{
+		return binarySearchHelper(arr,target,0,arr.length-1);
+	}
+	public static int binarySearchHelper(int[] arr,int target,int begin,int end)
+	{
+		if(begin>end)
+		{
+			return -1; //elemnet is not found in array
+		}
+		int mid = (begin+end)/2;
+		if(arr[mid] == target)
+		{
+			return mid; //element is found at index mid
+		}
+		else if(arr[mid]>target)
+		{
+			return binarySearchHelper(arr,target,begin,mid-1); // element is present in left halve
+		}
+		else
+		{
+           return binarySearchHelper(arr,target,mid+1,end); // element is present in right halve
+		}
+
+
+	}
+}
